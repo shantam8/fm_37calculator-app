10ef3b00a51052517e2f879b5fd43e905f697496
shantam8 <dscharmen@aol.com>
2021-09-26 13:38:12 +0200

    theme switcher done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,46 @@
-function init(){
-    console.log("hi");
+let body = document.querySelector("body");
+let themeSelector = document.querySelector("#themeSelector");
+
+let displayText = document.querySelector("#displayText");
+
+let btnCalc = document.querySelector("#btn_calc");
+let btnsFunct = document.querySelectorAll(".btn_funct");
+let btnsMainPad = document.querySelectorAll(".btn_mainPad");
+
+function setColorTheme(themeNr) {
+  body.classList.remove("theme1");
+  body.classList.remove("theme2");
+  body.classList.remove("theme3");
+  body.classList.add("theme" + themeNr);
 }
 
+function calculateResult(event) {
+  console.log(event.target);
+}
+
+function clearInput(event) {
+  console.log(event.target);
+}
+
+function handleMainPadInput(event) {
+  console.log(event.target);
+}
+
+function init() {
+  setColorTheme(2);
+  themeSelector.addEventListener("change", (event) => {
+    setColorTheme(event.target.value);
+  });
+
+  btnCalc.addEventListener("click", calculateResult);
+
+  btnsFunct.forEach((btn) => {
+    btn.addEventListener("click", clearInput);
+  });
+
+  btnsMainPad.forEach((btn) => {
+    btn.addEventListener("click", handleMainPadInput);
+  });
+}
 
 window.onload = init;
